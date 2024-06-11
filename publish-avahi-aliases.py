370e0ac2531c7646133669f0c4b1fb7cf939f25e
Nick <nick.p.doyle@gmail.com>
2024-06-11 20:15:46 +1000

    Update publish-avahi-aliases.py

--- publish-avahi-aliases.py
+++ publish-avahi-aliases.py
@@ -3,7 +3,7 @@ import subprocess
 
 ip = subprocess.check_output("ifconfig ens1f2 | grep 'inet ' | awk '{print $2}'", shell=True).strip().decode('utf8')
 
-with open('/run/avahi-daemon/avahi-aliases', 'r') as f:
+with open('/etc/avahi/avahi-aliases', 'r') as f:
     for line in f.readlines():
       hostname = line.strip()
       if hostname:
